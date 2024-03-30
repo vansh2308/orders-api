e0bb055545f4790f250136f8979be5040e7e1e89
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-03-31 03:03:05 +0530

    Refactor routes

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/vansh2308/orders-api/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -15,5 +16,18 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
+
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/vansh2308/orders-api.git
+module github.com/vansh2308/orders-api
 
 go 1.22.1
 
-require github.com/go-chi/chi v1.5.5 // indirect
+require github.com/go-chi/chi v1.5.5
