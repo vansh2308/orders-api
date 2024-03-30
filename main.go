f651996242c826b599a9cdbdf31cc3297d8f7f2a
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-03-31 02:46:25 +0530

    Makes application

--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/vansh2308/orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
+	err := app.Start(context.TODO())
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("Failed to start app:", err)
 	}
 
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hi nehal!"))
 }
